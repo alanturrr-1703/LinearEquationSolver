9a53463af1cda1d60254e7f4263f923287e6bd27
Harshit Thakkar <hariom.nthakkar@gmail.com>
2025-03-21 14:02:11 +0530

    Made it little cool by taking in inputs for any amount of equations that a user would want to solve

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Step 1: Get the number of variables
+        System.out.print("Enter the number of variables: ");
+        int numVariables = scanner.nextInt();
+        scanner.nextLine();  // Consume the newline character
+
+        Set<String> variables = new LinkedHashSet<>();
+        List<LinearEquation> equations = new ArrayList<>();
+
+        // Step 2: Input equations
+        System.out.println("Enter your equations (Example: 2x + 3y - z = 5):");
+        for (int i = 0; i < numVariables; i++) {
+            System.out.print("Equation " + (i + 1) + ": ");
+            String equation = scanner.nextLine();
+            equations.add(new LinearEquation(equation, variables));
+        }
+
+        // Step 3: Convert to coefficient matrix
+        CoefficientMatrix matrix = new CoefficientMatrix(equations, variables);
+
+        // Display coefficient matrix
+        matrix.printMatrix();
+
+        // Step 4: Solve the system
+        try {
+            Solver solver = new Solver(matrix);
+            double[] solution = solver.solve();
+            List<String> variableList = matrix.getVariables();
+
+            System.out.println("\nSolution:");
+            for (int i = 0; i < solution.length; i++) {
+                System.out.println(variableList.get(i) + " = " + solution[i]);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
